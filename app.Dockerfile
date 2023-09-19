ccefd20c042ebc59ad0199d5a06d1a0b2b4105b7
wildananugrah <wildananugrah@gmail.com>
2023-09-19 09:38:04 +0700

    update

--- app.Dockerfile
+++ app.Dockerfile
@@ -1,11 +1,9 @@
-FROM oven/bun
-# FROM node:18-alpine
+# FROM oven/bun
+FROM node:18-alpine
 
 WORKDIR /app
 COPY ./ .
-RUN bun install
-RUN bun prisma generate
-RUN bun prisma migrate dev --name user_email_unique
+RUN npm install
 EXPOSE 3000
-CMD ["bun", "--hot", "run" ,"index.js"]
-# CMD ["npm", "run" ,"dev"]
+
+CMD ["npm", "run" ,"dev"]
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import path from 'path';
 import { appHost, appPort, appEnv } from './configs/common.config.js';
+import { fileURLToPath } from 'url';
 
 import Server from 'fastify';
 const app = new Server({ logger: appEnv === 'development' ? true : false });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // register cors plugin
 import cors from '@fastify/cors';
